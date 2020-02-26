8b1d49c83eeb1e29f390584966aad6403cb5b805
Jean-Philippe Belley <jean-philippe.belley@cossette.com>
2020-02-25 21:38:12 -0500

    Basic Layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,22 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.scss" 
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#0E0C38`,
+      color: `#fff`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <div className="nav-title">Portfolio</div>
+    <ul>
+      <li>About</li>
+      <li>Portfolio</li>
+      <li>Contact</li>
+    </ul>
   </header>
 )
 
